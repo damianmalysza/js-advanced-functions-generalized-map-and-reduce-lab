a66a0755f295bc151c9d0e85cb7ac595ff147cef
Damian Malysza <damian.malysza@gmail.com>
2021-11-06 16:34:37 -0400

    commit tree ad0db248fa733bd704f279804e14b66f07118a9b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, fn){
+  let rtnArray = []
+  for (let item of array){
+    rtnArray.push(fn(item))
+  }
+  return rtnArray
+}
+
+function reduce(array,fn,startingPoint = 0){
+  let end = startingPoint
+
+  if (!startingPoint){
+    end = array[0]
+    array = array.slice(1)
+  }
+
+  for (let item of array){
+    end = fn(item,end)
+  }
+  return end
+}
